ef2d267a74c4a90b3449e045e0ca9edc6c4cc7b3
Swetha Udayagiri <106729744+SwethaUdayagiri@users.noreply.github.com>
2023-11-12 22:50:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function isSameType(value1, value2) {
 	if(value1==NaN && value2==NaN){
 		return true;
 	}
+	else if(value1==NaN||value2==NaN){
+		return false;
+	}	
 	else if(typeof(value1)==typeof(value2)){
 		return true;
 	}
@@ -10,6 +13,6 @@ function isSameType(value1, value2) {
 }
 
 // do not change the code below.
-let value1 = prompt("Enter Start of the Range.");
-let value2 = prompt("Enter End Of the Range.");
-alert(isSameType(value1, value2));
+//let value1 = prompt("Enter Start of the Range.");
+//let value2 = prompt("Enter End Of the Range.");
+//alert(isSameType(value1, value2));
